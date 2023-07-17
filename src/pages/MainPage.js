ba3c07527310fce8bb11666d71b32fbdcfe74535
raidBer <Kr_berrahal@esi.dz>
2023-07-17 01:07:10 +0100

    ready to deploy

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,14 +1,10 @@
 import SearchBar from "../components/SearchBar.js";
 import Dropdown from "../components/DropDown.js";
 import FlagSection from "../components/FlagSection.js";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 function MainPage({ countries, loading, err, refetch }) {
   const [region, changeRegion] = useState("all");
   const [textSearch, changeTextSearch] = useState("");
-
-  // useEffect(() => {
-  //   if (err) refetch;
-  // }, []);
 
   return (
     <div className="bg-veryDarkBlue transition-all duration-300 dark:bg-veryLightGray py-10 w-full min-h-screen z-0">
